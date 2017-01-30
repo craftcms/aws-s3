1723e050041cae151768efde648af4564e0e57cd
andris-sevcenko <asevcenko@gmail.com>
2017-01-30 16:12:22 +0200

    Asset bundle.

---
+++ src/AwsS3Bundle.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @link      https://craftcms.com/
+ * @copyright Copyright (c) Pixel & Tonic, Inc.
+ * @license   https://craftcms.com/license
+ */
+
+namespace craft\awss3;
+
+use craft\web\AssetBundle;
+use craft\web\assets\cp\CpAsset;
+
+/**
+ * Asset bundle for the Dashboard
+ */
+class AwsS3Bundle extends AssetBundle
+{
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        $this->sourcePath = '@craft/awss3/resources';
+
+        $this->depends = [
+            CpAsset::class,
+        ];
+
+        $this->css = [
+            'image_editor.css',
+        ];
+
+        $this->js = [
+            'js/editVolume.js',
+        ];
+
+        parent::init();
+    }
+}

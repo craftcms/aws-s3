ed0f69146db3ed395c73b31c8467e5d79c11be24
Brad Bell <brad@pixelandtonic.com>
2025-03-26 11:16:00 -0700

    try without overriding execute since it calls executeAsync anyway.

--- src/S3Client.php
+++ src/S3Client.php
@@ -48,29 +48,6 @@ class S3Client extends AwsS3Client
     /**
      * @inheritdoc
      */
-    public function execute(CommandInterface $command)
-    {
-        try {
-            // Just try to execute
-            return $this->_wrappedClient->execute($command);
-        } catch (S3Exception $exception) {
-            // Attempt to get new credentials
-            if ($exception->getAwsErrorCode() == 'ExpiredToken') {
-                $clientConfig = call_user_func($this->_generateNewConfig);
-                $this->_wrappedClient = new parent($clientConfig);
-
-                // Re-create the command to use the new credentials
-                $newCommand = $this->getCommand($command->getName(), $command->toArray());
-                return $this->_wrappedClient->execute($newCommand);
-            }
-
-            throw $exception;
-        }
-    }
-
-    /**
-     * @inheritdoc
-     */
     public function executeAsync(CommandInterface $command)
     {
         try {

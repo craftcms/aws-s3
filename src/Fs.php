186d91cb0cb421515beecfc159ca3ea38ca09df3
i-just <iwona@pixelandtonic.com>
2025-06-05 08:20:04 +0100

    Revert "call invalidate cdn path on write and writeFileFromStream"
    This reverts commit 1eb3f145d77396f676f2c64e754e02370919fe39.

--- src/Fs.php
+++ src/Fs.php
@@ -343,26 +343,6 @@ class Fs extends FlysystemFs
     /**
      * @inheritdoc
      */
-    public function write(string $path, string $contents, array $config = []): void
-    {
-        parent::write($path, $contents, $config);
-
-        $this->invalidateCdnPath($path);
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function writeFileFromStream(string $path, $stream, array $config = []): void
-    {
-        parent::writeFileFromStream($path, $stream, $config);
-
-        $this->invalidateCdnPath($path);
-    }
-
-    /**
-     * @inheritdoc
-     */
     protected function invalidateCdnPath(string $path): bool
     {
         if (!empty($this->cfDistributionId)) {
